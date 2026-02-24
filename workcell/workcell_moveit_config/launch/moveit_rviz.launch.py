bc9b39f762e22f57b48945bbb257615e3a9fb584
Daniel Csernyi <daneceni@gmail.com>
2026-02-24 21:27:17 +0100

    added use_sime_time arg for webots simulation, default = false

--- workcell/workcell_moveit_config/launch/moveit_rviz.launch.py
+++ workcell/workcell_moveit_config/launch/moveit_rviz.launch.py
@@ -1,7 +1,28 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_moveit_rviz_launch
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import SetParameter
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("workcell", package_name="workcell_moveit_config").to_moveit_configs()
-    return generate_moveit_rviz_launch(moveit_config)
+    moveit_config = MoveItConfigsBuilder(
+        "workcell", package_name="workcell_moveit_config"
+    ).to_moveit_configs()
+
+    ld = generate_moveit_rviz_launch(moveit_config)
+
+    ld.entities.insert(
+        0,
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="false",
+            description="Use simulation time from /clock",
+        ),
+    )
+    ld.entities.insert(
+        1,
+        SetParameter(name="use_sim_time", value=LaunchConfiguration("use_sim_time")),
+    )
+
+    return ld
